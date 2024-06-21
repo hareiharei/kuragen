2ba930d0e6f20ad185bbca98506d8b1fce81fcb1
harei.whitewings@gmail.com <harei.whitewings@gmail.com>
2024-06-22 03:47:55 +0900

    SQLAlchemyでデータベース接続

---
+++ backend/app/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+'''
+
+参考にしたサイト:
+https://fastapi.tiangolo.com/tutorial/sql-databases/
+
+'''
