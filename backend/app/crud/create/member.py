b943852d5edfa3796e1c5b2fe85195853501e258
harei.whitewings@gmail.com <harei.whitewings@gmail.com>
2024-06-22 05:37:07 +0900

    ORMの書き方に変更

--- backend/app/crud/create/member.py
+++ backend/app/crud/create/member.py
@@ -1,21 +1,40 @@
 from sqlalchemy.orm import Session
 from ... import models, schemas
 
-def create_member(db: Session, member: schemas.MemberCreate):
-    db.execute(
-        "INSERT INTO members (email, password, last_name, first_name, generation) \
-         VALUES (:email, :password, :last_name, :first_name, :generation)",
-        {
-            'email': member.email,
-            'password': member.password,
-            'last_name': member.last_name,
-            'first_name': member.first_name,
-            'generation': member.generation
-        }
-    )
+def create_member(db: Session, creating_member: schemas.MemberCreate):
+    # メンバーを作成
+    # db.execute(
+    #     """
+    #     INSERT INTO members
+    #     (email, password, last_name, first_name, generation)
+    #     VALUES
+    #     (:email, :password, :last_name, :first_name, :generation)
+    #     """,
+    #     {
+    #         'email': creating_member.email,
+    #         'password': creating_member.password,
+    #         'last_name': creating_member.last_name,
+    #         'first_name': creating_member.first_name,
+    #         'generation': creating_member.generation
+    #     }
+    # )
+    db_member = models.Member.model_validate(creating_member)
     db.commit()
+    db.refresh(db_member)
+    return db_member
 
-'''
-参考にしたサイト：
-https://fastapi.tiangolo.com/tutorial/sql-databases/#__tabbed_1_1
-'''
+    # 新しく作成されたメンバーを取得
+    # result = db.execute(
+    #     """
+    #     "SELECT * FROM members 
+    #      WHERE email = :email"
+    #     """,
+    #     {'email': member.email}
+    # )
+
+    # return result.fetchone()
+
+
+# 参考にしたサイト：
+# https://fastapi.tiangolo.com/tutorial/sql-databases/#__tabbed_1_1
+# https://github.com/tiangolo/full-stack-fastapi-template/blob/master/backend/app/crud.py
